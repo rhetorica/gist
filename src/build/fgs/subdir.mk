467906e3c1f4df810f862b932139457f4570a978
Samantha Halliday <rhetorica@cs.toronto.edu>
2016-11-29 03:42:44 -0500

    Switched makefiles from icpc to g++.

--- src/build/fgs/subdir.mk
+++ src/build/fgs/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 fgs/%.o: ../fgs/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	icpc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
